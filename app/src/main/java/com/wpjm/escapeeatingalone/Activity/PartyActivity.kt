52478d545b7816517f173ade2ac588eb76e8db09
jaewoo-Lim <limjaewoo0101@gmail.com>
2021-04-29 20:47:38 +0900

    메뉴 파티원 생성

--- app/src/main/java/com/wpjm/escapeeatingalone/Activity/PartyActivity.kt
+++ app/src/main/java/com/wpjm/escapeeatingalone/Activity/PartyActivity.kt
@@ -1,16 +1,20 @@
 package com.wpjm.escapeeatingalone.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
 import com.wpjm.escapeeatingalone.Model.PartyModel
 import com.wpjm.escapeeatingalone.R
-import com.wpjm.escapeeatingalone.databinding.ActivityMenuDetailBinding
 import com.wpjm.escapeeatingalone.databinding.ActivityPartyBinding
 
 class PartyActivity : AppCompatActivity() {
     private var mBinding: ActivityPartyBinding? = null
     private val binding get() = mBinding!!
+    private var fsdb = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +25,36 @@ class PartyActivity : AppCompatActivity() {
         var storeName = intent.getStringExtra("storeName")
         binding.partyActivityTextViewName.setText(storeName)
 
-        var partyList = arrayListOf(
-            PartyModel("4/17", "빨리갈 친구들 구함", "1/4"),
-            PartyModel("4/17", "빨리갈 친구들 구함", "1/4"),
-            PartyModel("4/17", "빨리갈 친구들 구함", "1/4"),
-            PartyModel("4/17", "빨리갈 친구들 구함", "1/4"),
-            PartyModel("4/17", "빨리갈 친구들 구함", "1/4")
-        )
+        var partyList = arrayListOf<PartyModel>()
+        var adapter = PartyAdapter(partyList)
+
+       fsdb.collection("party").get()
+               .addOnSuccessListener { result ->
+                   partyList.clear()
+                   for(document in result) {
+                       val item = PartyModel(
+                               document["date"] as String,
+                               document["title"] as String,
+                               document["count"] as String
+                       )
+                       partyList.add(item)
+                   }
+                   adapter.notifyDataSetChanged()
+               }
+               .addOnFailureListener { exception -> Log.e("X", "$exception") }
+
+        binding.partyActivityButtonWrite.setOnClickListener (View.OnClickListener {
+            gotoActivity(PartyMake::class.java)
+        })
 
         binding.partyActivityRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
         binding.partyActivityRecyclerView.setHasFixedSize(true)
-        binding.partyActivityRecyclerView.adapter = PartyAdapter(partyList)
+        binding.partyActivityRecyclerView.adapter = adapter
 
+    }
+
+    private fun gotoActivity(c: Class<*>) {
+        var intent = Intent(this, c)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/wpjm/escapeeatingalone/Model/PartyModel.kt
+++ app/src/main/java/com/wpjm/escapeeatingalone/Model/PartyModel.kt
@@ -1,3 +1,13 @@
 package com.wpjm.escapeeatingalone.Model
 
-class PartyModel (val date:String, val title: String, val count: String)
+class PartyModel {
+    var date : String = ""
+    var title : String = ""
+    var count : String = ""
+
+    constructor(date: String, title: String, count: String) {
+        this.date = date
+        this.title = title
+        this.count = count
+    }
+}
