ba8b7080fa19dd08301a53bf064f557b92456d53
jaewoo-Lim <limjaewoo0101@gmail.com>
2021-04-29 22:06:15 +0900

    메뉴 파티원 리스트 읽어오기

--- app/src/main/java/com/wpjm/escapeeatingalone/Activity/PartyActivity.kt
+++ app/src/main/java/com/wpjm/escapeeatingalone/Activity/PartyActivity.kt
@@ -4,17 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.wpjm.escapeeatingalone.Model.PartyModel
 import com.wpjm.escapeeatingalone.R
+import com.wpjm.escapeeatingalone.databinding.ActivityMenuDetailBinding
 import com.wpjm.escapeeatingalone.databinding.ActivityPartyBinding
 
 class PartyActivity : AppCompatActivity() {
     private var mBinding: ActivityPartyBinding? = null
     private val binding get() = mBinding!!
-    private var fsdb = FirebaseFirestore.getInstance()
+    private var db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,34 +25,40 @@ class PartyActivity : AppCompatActivity() {
         var storeName = intent.getStringExtra("storeName")
         binding.partyActivityTextViewName.setText(storeName)
 
+        // fireStore 읽어오기
         var partyList = arrayListOf<PartyModel>()
         var adapter = PartyAdapter(partyList)
 
-       fsdb.collection("party").get()
-               .addOnSuccessListener { result ->
-                   partyList.clear()
-                   for(document in result) {
-                       val item = PartyModel(
-                               document["date"] as String,
-                               document["title"] as String,
-                               document["count"] as String
-                       )
-                       partyList.add(item)
-                   }
-                   adapter.notifyDataSetChanged()
-               }
-               .addOnFailureListener { exception -> Log.e("X", "$exception") }
+        db.collection("party")
+                .whereEqualTo("storeName", "${storeName}")
+                .get()
+                .addOnSuccessListener { result ->
+                    partyList.clear()
+                    for(document in result) {
+                        val item = PartyModel(
+                                document["date"] as String,
+                                document["title"] as String,
+                                document["count"] as String,
+                                document["storeName"] as String
+                        )
+                        partyList.add(item)
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("X", "$exception")
+                }
 
-        binding.partyActivityButtonWrite.setOnClickListener (View.OnClickListener {
-            gotoActivity(PartyMake::class.java)
-        })
+        binding.partyActivityButtonWrite.setOnClickListener {
+            gotoActivity(PartyMakeActivity::class.java)
+        }
 
         binding.partyActivityRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
         binding.partyActivityRecyclerView.setHasFixedSize(true)
         binding.partyActivityRecyclerView.adapter = adapter
-
     }
 
+    // Intent function
     private fun gotoActivity(c: Class<*>) {
         var intent = Intent(this, c)
         startActivity(intent)
--- app/src/main/java/com/wpjm/escapeeatingalone/Model/PartyModel.kt
+++ app/src/main/java/com/wpjm/escapeeatingalone/Model/PartyModel.kt
@@ -1,13 +1,3 @@
 package com.wpjm.escapeeatingalone.Model
 
-class PartyModel {
-    var date : String = ""
-    var title : String = ""
-    var count : String = ""
-
-    constructor(date: String, title: String, count: String) {
-        this.date = date
-        this.title = title
-        this.count = count
-    }
-}
+class PartyModel (val date: String, val title: String, val count: String, val storeName: String)
