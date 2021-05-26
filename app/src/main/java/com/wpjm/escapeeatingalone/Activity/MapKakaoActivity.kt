3093ea09d8bdf44347b9a6e82d7c49cdfdfa32a1
goongju <titu1015@gmail.com>
2021-05-26 21:01:25 +0900

    KAKAOMAP and MemberModify

--- app/src/main/java/com/wpjm/escapeeatingalone/Activity/MapKakaoActivity.kt
+++ app/src/main/java/com/wpjm/escapeeatingalone/Activity/MapKakaoActivity.kt
@@ -1,28 +1,140 @@
 package com.wpjm.escapeeatingalone.Activity
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.wpjm.escapeeatingalone.Adapter.ListAdapter
+import com.wpjm.escapeeatingalone.Interface.KakaoAPI
+import com.wpjm.escapeeatingalone.Model.ListLayout
+import com.wpjm.escapeeatingalone.Model.ResultSearchKeyword
 import com.wpjm.escapeeatingalone.R
 import com.wpjm.escapeeatingalone.databinding.ActivityKakaoMapBinding
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
 
 class MapKakaoActivity : AppCompatActivity() {
-    private var mBinding: ActivityKakaoMapBinding? = null
-    private val binding get() = mBinding!!
+    companion object {
+        const val BASE_URL = "https://dapi.kakao.com/"
+        const val API_KEY = "24332e6fac5132ea5dc1a303bee1707e"  // REST API 키
+    }
+
+    private lateinit var binding : ActivityKakaoMapBinding
+    private val listItems = arrayListOf<ListLayout>()   // 리사이클러 뷰 아이템
+    private val listAdapter = ListAdapter(listItems)    // 리사이클러 뷰 어댑터
+    private var pageNumber = 1      // 검색 페이지 번호
+    private var keyword = ""        // 검색 키워드
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kakao_map)
-        mBinding = ActivityKakaoMapBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        val mapView = MapView(this)
+        binding = ActivityKakaoMapBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val mapViewContainer = binding.mapView
-        mapViewContainer.addView(mapView)
+        // 리사이클러 뷰
+        binding.rvList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.rvList.adapter = listAdapter
+        // 리스트 아이템 클릭 시 해당 위치로 이동
+        listAdapter.setItemClickListener(object: ListAdapter.OnItemClickListener {
+            override fun onClick(v: View, position: Int) {
+                val mapPoint = MapPoint.mapPointWithGeoCoord(listItems[position].y, listItems[position].x)
+                binding.mapView.setMapCenterPointAndZoomLevel(mapPoint, 1, true)
+            }
+        })
 
+        // 검색 버튼
+        binding.btnSearch.setOnClickListener {
+            keyword = binding.etSearchField.text.toString()
+            pageNumber = 1
+            searchKeyword(keyword, pageNumber)
+        }
+
+        // 이전 페이지 버튼
+        binding.btnPrevPage.setOnClickListener {
+            pageNumber--
+            binding.tvPageNumber.text = pageNumber.toString()
+            searchKeyword(keyword, pageNumber)
+        }
+
+        // 다음 페이지 버튼
+        binding.btnNextPage.setOnClickListener {
+            pageNumber++
+            binding.tvPageNumber.text = pageNumber.toString()
+            searchKeyword(keyword, pageNumber)
+        }
+    }
+
+    // 키워드 검색 함수
+    private fun searchKeyword(keyword: String, page: Int) {
+        val retrofit = Retrofit.Builder()          // Retrofit 구성
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val api = retrofit.create(KakaoAPI::class.java)            // 통신 인터페이스를 객체로 생성
+        val call = api.getSearchKeyword(API_KEY, keyword, page)    // 검색 조건 입력
+
+        // API 서버에 요청
+        call.enqueue(object: Callback<ResultSearchKeyword> {
+            override fun onResponse(call: Call<ResultSearchKeyword>, response: Response<ResultSearchKeyword>) {
+                // 통신 성공
+                addItemsAndMarkers(response.body())
+                Log.d("LocalSearch", "통신 성공")
+            }
+
+            override fun onFailure(call: Call<ResultSearchKeyword>, t: Throwable) {
+                // 통신 실패
+                Log.w("LocalSearch", "통신 실패: ${t.message}")
+            }
+        })
+    }
+
+    // 검색 결과 처리 함수
+    private fun addItemsAndMarkers(searchResult: ResultSearchKeyword?) {
+        if (!searchResult?.documents.isNullOrEmpty()) {
+            // 검색 결과 있음
+            listItems.clear()                   // 리스트 초기화
+            binding.mapView.removeAllPOIItems() // 지도의 마커 모두 제거
+            for (document in searchResult!!.documents) {
+                // 결과를 리사이클러 뷰에 추가
+                val item = ListLayout(document.place_name,
+                    document.road_address_name,
+                    document.address_name,
+                    document.x.toDouble(),
+                    document.y.toDouble())
+                listItems.add(item)
+
+                // 지도에 마커 추가
+                val point = MapPOIItem()
+                point.apply {
+                    itemName = document.place_name
+                    mapPoint = MapPoint.mapPointWithGeoCoord(document.y.toDouble(),
+                        document.x.toDouble())
+                    markerType = MapPOIItem.MarkerType.BluePin
+                    selectedMarkerType = MapPOIItem.MarkerType.RedPin
+                }
+                binding.mapView.addPOIItem(point)
+            }
+            listAdapter.notifyDataSetChanged()
+
+            binding.btnNextPage.isEnabled = !searchResult.meta.is_end // 페이지가 더 있을 경우 다음 버튼 활성화
+            binding.btnPrevPage.isEnabled = pageNumber != 1             // 1페이지가 아닐 경우 이전 버튼 활성화
+
+        } else {
+            // 검색 결과 없음
+            Toast.makeText(this, "검색 결과가 없습니다", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
