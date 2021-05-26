b349871117b4d7923c996448208e9958a0a92061
limsangun <tkrnfldlekd1@naver.com>
2021-05-26 10:59:38 +0900

    친구리스트프로필사진

--- app/src/main/java/com/wpjm/escapeeatingalone/Model/MenuDetailModel.kt
+++ app/src/main/java/com/wpjm/escapeeatingalone/Model/MenuDetailModel.kt
@@ -1,3 +1,19 @@
 package com.wpjm.escapeeatingalone.Model
 
 class MenuDetailModel (val logo:Int, val name:String, val explanation:String, val type:String)
+//class MenuDetailModel {
+//    private var bsnsNm: String? = null
+//
+//
+//    constructor(bsnsNm: String){
+//        this.bsnsNm = bsnsNm
+//    }
+//
+//    fun setbsnsNm(bsnsNm: String) {
+//        this.bsnsNm = bsnsNm
+//    }
+//
+//    fun getbsnsNm(): String? {
+//        return this.bsnsNm
+//    }
+//}
